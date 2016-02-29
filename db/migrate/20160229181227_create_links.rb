76f1d7df58b259a2ae58c80fb7958ea6a1b3ef85
Tyler Komoroske <tjkomor@clemson.edu>
2016-02-29 12:08:32 -0700

    add read column to links

--- db/migrate/20160229181227_create_links.rb
+++ db/migrate/20160229181227_create_links.rb
@@ -3,7 +3,7 @@ class CreateLinks < ActiveRecord::Migration
     create_table :links do |t|
       t.string :title
       t.string :url
-
+      t.boolean :read, default: false
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20160229181227) do
   create_table "links", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "read",       default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
