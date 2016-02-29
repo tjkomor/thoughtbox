ff15fcbbab99578bddf8e0304f4910f49bbd1a5a
Tyler Komoroske <tjkomor@clemson.edu>
2016-02-29 14:32:21 -0700

    implemented API and ajax to successfully change status

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#delete"
+
+  namespace :api do
+    namespace :v1 do
+      resources :links, only: [:update]
+    end
+  end
 end
