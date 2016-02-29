ece1fe39be60b61d0f7d4cb40be85accd61ae5b9
Tyler Komoroske <tjkomor@clemson.edu>
2016-02-29 09:34:20 -0700

    user model updated with password_digest

--- db/migrate/20160229162745_create_users.rb
+++ db/migrate/20160229162745_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :email_address
-
+      t.string :password_digest
       t.timestamps null: false
     end
   end
