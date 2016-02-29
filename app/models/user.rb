3e215290ffef4c81b8004e62946d07ee71011d09
Tyler Komoroske <tjkomor@clemson.edu>
2016-02-29 09:45:31 -0700

    user model tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, :username, :password, presence: true
-  validates :email, confirmation: true, uniqueness: true
+  validates :email_address, :username, :password, presence: true
+  validates :email_address, confirmation: true, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user is valid with email, username, and password" do
+    user = User.new(username: 'tyler',
+                    password: "password",
+                    email_address: "tyler@tyler.com")
+    assert_equal user.username, 'tyler'
+    assert_equal user.password, 'password'
+    assert_equal user.email_address, 'tyler@tyler.com'
+    assert user.valid?
+  end
+
+  test 'user is invalid without email_address' do
+    user = User.new(username: 'tyler',
+                    password: "password")
+    assert user.invalid?
+  end
+
+  test 'user is invalid without username' do
+    user = User.new(email_address: 'tyler@tyler.com',
+                    password: "password")
+    assert user.invalid?
+  end
+
+  test 'user is invalid without password' do
+    user = User.new(email_address: 'tyler@tyler.com',
+                    username: "tyler")
+    assert user.invalid?
+  end
 end
