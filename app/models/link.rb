976aff7aa6f9aa1917e2a76b3f9e18facd0ae433
Tyler Komoroske <tjkomor@clemson.edu>
2016-02-29 12:53:24 -0700

    user cannot submit invalid link cannot get error message to pass in test as of now

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,14 @@
+require 'uri'
 class Link < ActiveRecord::Base
+  include ActiveModel::Validations
+  validate :valid_link?
+
+  validates :url, :title, presence: true, uniqueness: true
+
+  def valid_link?
+    uri = URI.parse(self.url)
+    errors.add(:base, "Please submit a valid URL") unless uri.kind_of?(URI::HTTP)
+  rescue URI::InvalidURIError
+    false
+  end
 end
